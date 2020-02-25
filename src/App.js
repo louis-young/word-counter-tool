0d9e4f6cb8027de6991a845c34565c6c9871a809
louis-yng <me@louisyoung.co.uk>
2020-02-25 13:45:57 +0000

    Adding files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./stylesheets/main.scss";
+
+/**
+ * Application component.
+ */
+
+const App = () => {
+  return <div className="App"></div>;
+};
+
+export default App;
